e2006643c506178deec9a079fc4b03ac7c3ad9a0
garyrobert95 <72111607+garyrobert95@users.noreply.github.com>
2020-10-01 18:10:32 -0300

    Add files via upload

---
+++ Procedimientos almacenados/SP_CREAR_USUARIO.sql
@@ -0,0 +1,37 @@
+create or replace PROCEDURE SP_CREAR_USUARIO(
+  
+  IN_CONTRASENA 		IN VARCHAR2,
+  IN_CORREO 			IN VARCHAR2,
+  IN_ROL_ID 			IN NUMBER,
+  OUT_GLOSA 		OUT VARCHAR2,
+  OUT_ESTADO 		OUT NUMBER,
+  OUT_ID_SALIDA 	OUT NUMBER
+) AS
+
+
+BEGIN
+
+    OUT_GLOSA := 'SP_CREAR_USUARIO ejecutado exitosamente';
+    OUT_ESTADO := 0;
+ 
+   INSERT INTO feria2.usuario(
+     -- ID,
+      CONTRASENA,
+      HABILITADO,
+      correo,
+      rol_id,
+   )
+    VALUES (
+      -- feria2.USUARIO_SEQUENCE.NEXTVAL,
+       IN_CONTRASENA,
+       1,
+       IN_CORREO,
+       IN_ROL_ID,
+    )
+    RETURNING ID INTO OUT_ID_SALIDA;
+
+    EXCEPTION
+        WHEN OTHERS THEN
+            OUT_ESTADO := -1;
+            OUT_GLOSA := feria2.FN_GET_GLOSA_ERROR;
+END SP_CREAR_USUARIO;
