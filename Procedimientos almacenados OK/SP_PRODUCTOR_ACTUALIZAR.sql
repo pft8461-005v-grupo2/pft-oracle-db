41694238151e3b3ed2e4b5d07501257a78ddb0f8
Yandiel Lillo <ylillog@gmail.com>
2020-11-05 01:43:25 -0300

    Se actualizar script db 1.3 sacando el contrato_id de productor y se actualiza INSERT.sql

---
+++ Procedimientos almacenados OK/SP_PRODUCTOR_ACTUALIZAR.sql
@@ -0,0 +1,68 @@
+CREATE OR REPLACE PROCEDURE SP_PRODUCTOR_ACTUALIZAR (
+
+    IN_ID_PRODUCTOR     IN NUMBER,
+    IN_CONTRATO_ID      IN NUMBER,
+    IN_RUT              IN VARCHAR2,
+    IN_RAZONSOCIAL      IN VARCHAR2,
+    IN_DIRECCION        IN VARCHAR2,
+    IN_COMUNA           IN VARCHAR2,
+    IN_CORREO           IN VARCHAR2,
+    IN_HABILITADO       IN NUMBER,
+    IN_USUARIO_ID       IN NUMBER,
+
+    OUT_GLOSA 	    OUT VARCHAR2,
+    OUT_ESTADO 	    OUT NUMBER,
+    OUT_ID_SALIDA   OUT NUMBER
+    )AS 
+    
+    EX_Sin_data                 EXCEPTION;
+    tiene_datos     NUMBER := 0;
+    
+BEGIN
+
+    OUT_GLOSA := 'SP_PRODUCTOR_ACTUALIZAR ejecutado exitosamente';
+    OUT_ESTADO := 0;
+    
+    SELECT
+        COUNT(1)
+    INTO tiene_datos
+    FROM PRODUCTOR product
+    WHERE product.ID = IN_ID_PRODUCTOR;
+
+    IF tiene_datos = 0 THEN
+        RAISE EX_Sin_data;
+    END IF;
+    
+    UPDATE PRODUCTOR 
+    SET
+    PRODUCTOR.CONTRATO_ID = NVL2( IN_CONTRATO_ID , IN_CONTRATO_ID, PRODUCTOR.CONTRATO_ID  ),
+    PRODUCTOR.RUT = NVL2( IN_RUT , IN_RUT, PRODUCTOR.RUT  ),
+    PRODUCTOR.RAZONSOCIAL = NVL2( IN_RAZONSOCIAL , IN_RAZONSOCIAL, PRODUCTOR.RAZONSOCIAL  ),
+    PRODUCTOR.DIRECCION = NVL2( IN_DIRECCION , IN_DIRECCION, PRODUCTOR.DIRECCION  ),
+    PRODUCTOR.COMUNA = NVL2( IN_COMUNA , IN_COMUNA, PRODUCTOR.COMUNA  ),
+    PRODUCTOR.CORREO = NVL2( IN_CORREO , IN_CORREO, PRODUCTOR.CORREO  ),
+    PRODUCTOR.HABILITADO = NVL2( IN_HABILITADO , IN_HABILITADO, PRODUCTOR.HABILITADO  )
+    WHERE ID=IN_ID_PRODUCTOR ;
+    
+    
+    IF IN_CORREO != null THEN
+        MERGE INTO usuario usu
+        USING ( SELECT * from productor) pro
+        ON (usu.id=pro.usuario_id)
+        WHEN MATCHED THEN
+        UPDATE SET
+          usu.correo=pro.correo;
+     END IF;     
+    
+    EXCEPTION
+    
+        WHEN EX_Sin_data THEN
+                OUT_GLOSA := 'SP_PRODUCTOR_ACTUALIZAR no se encuentra id';
+                OUT_ESTADO := -1;
+                OUT_ID_SALIDA := -1;
+                
+        WHEN OTHERS THEN
+            OUT_ESTADO := -1;
+            OUT_GLOSA := FN_GET_GLOSA_ERROR;
+
+END SP_PRODUCTOR_ACTUALIZAR;
