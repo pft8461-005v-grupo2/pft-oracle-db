324e64823d44052f9809f3109278315d36500491
garyrobert95 <72111607+garyrobert95@users.noreply.github.com>
2020-11-06 23:55:57 -0300

    Update SP_PRODUCTOR_ACTUALIZAR.sql

--- Procedimientos almacenados OK/SP_PRODUCTOR_ACTUALIZAR.sql
+++ Procedimientos almacenados OK/SP_PRODUCTOR_ACTUALIZAR.sql
@@ -1,4 +1,4 @@
-CREATE OR REPLACE PROCEDURE SP_PRODUCTOR_ACTUALIZAR (
+create or replace PROCEDURE SP_PRODUCTOR_ACTUALIZAR (
 
     IN_ID_PRODUCTOR     IN NUMBER,
     IN_RUT              IN VARCHAR2,
@@ -16,12 +16,12 @@ CREATE OR REPLACE PROCEDURE SP_PRODUCTOR_ACTUALIZAR (
     
     EX_Sin_data                 EXCEPTION;
     tiene_datos     NUMBER := 0;
-    
+
 BEGIN
 
     OUT_GLOSA := 'SP_PRODUCTOR_ACTUALIZAR ejecutado exitosamente';
     OUT_ESTADO := 0;
-    
+
     SELECT
         COUNT(1)
     INTO tiene_datos
@@ -31,7 +31,7 @@ BEGIN
     IF tiene_datos = 0 THEN
         RAISE EX_Sin_data;
     END IF;
-    
+
     UPDATE PRODUCTOR 
     SET
     PRODUCTOR.RUT = NVL2( IN_RUT , IN_RUT, PRODUCTOR.RUT  ),
@@ -41,24 +41,33 @@ BEGIN
     PRODUCTOR.CORREO = NVL2( IN_CORREO , IN_CORREO, PRODUCTOR.CORREO  ),
     PRODUCTOR.HABILITADO = NVL2( IN_HABILITADO , IN_HABILITADO, PRODUCTOR.HABILITADO  )
     WHERE ID=IN_ID_PRODUCTOR ;
-    
-    
-    IF IN_CORREO != null THEN
+
+
+    --IF IN_CORREO != null THEN
+      --  MERGE INTO usuario usu
+      --  USING ( SELECT * from productor) pro
+      --  ON (usu.id=pro.usuario_id)
+      --  WHEN MATCHED THEN
+      --  UPDATE SET
+     --     usu.correo=pro.correo;
+    -- END IF;     
+     
+       --IF IN_CORREO != null THEN
         MERGE INTO usuario usu
-        USING ( SELECT * from productor) pro
+        USING ( SELECT correo, id,usuario_id from productor) pro
         ON (usu.id=pro.usuario_id)
         WHEN MATCHED THEN
         UPDATE SET
-          usu.correo=pro.correo;
-     END IF;     
-    
+          usu.correo=pro.correo where pro.id=IN_ID_PRODUCTOR ;
+     --END IF;  
+
     EXCEPTION
-    
+
         WHEN EX_Sin_data THEN
                 OUT_GLOSA := 'SP_PRODUCTOR_ACTUALIZAR no se encuentra id';
                 OUT_ESTADO := -1;
                 OUT_ID_SALIDA := -1;
-                
+
         WHEN OTHERS THEN
             OUT_ESTADO := -1;
             OUT_GLOSA := FN_GET_GLOSA_ERROR;
