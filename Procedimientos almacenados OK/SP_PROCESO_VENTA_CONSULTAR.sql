f5ed08f72dcd3188545944634a6758ca2029da99
Yandiel Lillo <ylillog@gmail.com>
2020-12-02 01:51:08 -0300

    Agregado SP consultar y actualizar de PROCESO_VENTA

--- Procedimientos almacenados OK/SP_PROCESO_VENTA_CONSULTAR.sql
+++ Procedimientos almacenados OK/SP_PROCESO_VENTA_CONSULTAR.sql
@@ -2,7 +2,6 @@ create or replace PROCEDURE SP_PROCESO_VENTA_CONSULTAR
 (
     -- Opcionales
     in_id                   IN NUMBER,
-    in_ingreso_id           IN NUMBER,
     in_solicitud_compra_id  IN NUMBER,
     in_subasta_id           IN NUMBER,
     in_etapa                IN NUMBER,
@@ -23,7 +22,6 @@ BEGIN
 
 
     select      id,
-                ingreso_id,
                 solicitud_compra_id,
                 subasta_id,
                 etapa,
@@ -35,15 +33,18 @@ BEGIN
     where 	
     		-- Opcionales
     		(in_id IS NULL OR id = in_id)
-			AND (in_ingreso_id IS NULL OR ingreso_id = in_ingreso_id)
 			AND (in_solicitud_compra_id IS NULL OR solicitud_compra_id = in_solicitud_compra_id)
 			AND (in_subasta_id IS NULL OR subasta_id = in_subasta_id)
 			AND (in_etapa IS NULL OR etapa = in_etapa)
-			AND (in_fechaCreacion IS NULL OR fechaCreacion = in_fechaCreacion)
+			AND (in_fechaCreacion IS NULL OR TRUNC(fechaCreacion) = TRUNC(in_fechaCreacion))
 			AND (in_clienteAceptaAcuerdo IS NULL OR clienteAceptaAcuerdo = in_clienteAceptaAcuerdo)
 			AND (in_precioVentaTotal IS NULL OR precioVentaTotal = in_precioVentaTotal)
 			AND (in_precioCostoTotal IS NULL OR precioCostoTotal = in_precioCostoTotal)
     ;
 
+EXCEPTION
+
+    WHEN OTHERS THEN
+		out_resultado := null;
 
 END SP_PROCESO_VENTA_CONSULTAR;
