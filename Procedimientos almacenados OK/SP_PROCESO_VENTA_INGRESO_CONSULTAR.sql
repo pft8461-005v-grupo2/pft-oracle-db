762a80a1796bce038978d1015fb6b36588d0e513
Yandiel Lillo <ylillog@gmail.com>
2020-12-08 19:33:32 -0300

    Scrip 1.8 y modificacion de SP PROCESO_VENTA_INGRESO

--- Procedimientos almacenados OK/SP_PROCESO_VENTA_INGRESO_CONSULTAR.sql
+++ Procedimientos almacenados OK/SP_PROCESO_VENTA_INGRESO_CONSULTAR.sql
@@ -1,4 +1,4 @@
-create or replace PROCEDURE SP_PROCESO_VENTA_INGRESO_CONSULTAR
+create or replace PROCEDURE SP_PR_VENTA_INGRESO_CONSULTAR
 (
     -- Opcionales
     IN_INGRESO_ID                   IN NUMBER,
@@ -11,25 +11,26 @@ AS
 
 BEGIN
 
-	-- Validador de valores obligatorios
+    -- Validador de valores obligatorios
     OPEN out_resultado FOR
 
 
     select
         INGRESO_ID,
         PROCESO_VENTA_ID,
+        KILOGRAMOSOCUPADOS,
         HABILITADO
     FROM PROCESO_VENTA_INGRESO
-    where 	
-    		-- Opcionales
-    		    (IN_INGRESO_ID IS NULL OR INGRESO_ID = IN_INGRESO_ID)
-			AND (IN_PROCESO_VENTA_ID IS NULL OR PROCESO_VENTA_ID = IN_PROCESO_VENTA_ID)
-			AND (IN_HABILITADO IS NULL OR HABILITADO = IN_HABILITADO)
+    where   
+            -- Opcionales
+                (IN_INGRESO_ID IS NULL OR INGRESO_ID = IN_INGRESO_ID)
+            AND (IN_PROCESO_VENTA_ID IS NULL OR PROCESO_VENTA_ID = IN_PROCESO_VENTA_ID)
+            AND (IN_HABILITADO IS NULL OR HABILITADO = IN_HABILITADO)
     ;
 
 EXCEPTION
 
     WHEN OTHERS THEN
-		out_resultado := null;
+        out_resultado := null;
 
-END SP_PROCESO_VENTA_INGRESO_CONSULTAR;
+END SP_PR_VENTA_INGRESO_CONSULTAR;
